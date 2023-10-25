a0ba24253dbe75e5d4dc4bd2356dbc280ef13270
Quan28032002 <hongquan28032002@gmail.com>
2023-10-25 14:17:17 +0700

    front-end special sprize ver2

--- frontend/src/components/Service/Service.jsx
+++ frontend/src/components/Service/Service.jsx
@@ -1,52 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import { FakeService } from '../../FakeData/Treatment';
-import { Link } from 'react-router-dom';
-import serviceDetailImg from '../../Images/service-details-promo1.png';
-import Services from '../../Pages/Home/Services/Services';
-import authService from '../../services/auth.service';
-import userService from '../../services/user.service';
+import axios from 'axios';  // Import axios for making API requests
+
 const Service = () => {
-
-
-    const [services, setServices] = useState([]);
-
-    const [isUser, setIsUser] = useState(false);
-    const [currentUser, setCurrentUser] = useState();
-
+    const [countSpecialPrizes, setCountSpecialPrizes] = useState({}); // State to hold the special prize data
 
     useEffect(() => {
-        async function fetchData() {
-            const response = await userService.getAllService();
-            console.log(response.data)
-            setServices(response.data);
-        }
-        fetchData();
-
-        const user = authService.getCurrentUser();
-        if (user) {
-            setIsUser(user.roles.includes("ROLE_USER"));
-            setCurrentUser(user)
-        }
-
+        // Make an API request to get the special prize data
+        axios.get('http://localhost:9999/special/special-long-to-long') // Replace YOUR_PORT_HERE with the correct port
+            .then((response) => {
+                setCountSpecialPrizes(response.data.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching special prizes data: ', error);
+            });
     }, []);
+
     return (
-        <section className="service-wrapper">
+        <section className="gallery-wrapper text-white">
             <Container>
                 <Row>
-                    <Col sm={12}>
-                        <div className="section-title text-center">
-                            <h1>Phân Phối Xác Suất</h1>
+                    <Col sm={12} className="text-center">
+                        <div className="section-title">
+                            <h1>Số ngày mà các giải đặc biệt lâu chưa ra</h1>
                         </div>
                     </Col>
                 </Row>
-                <Row>
-                    {
-                        services.map(service => (
-                            <Services key={service.id} service={service} />
-                        ))
-                    }
-                </Row>
+                <table className="tbl1" cellSpacing="1" cellPadding="4" style={{ border: "1px solid black" }}>
+                    <tbody>
+                        {Object.keys(countSpecialPrizes).map((specialPrize, index) => (
+                            <tr key={index}>
+                                <td style={{ border: "1px solid black" }} className="col1">{specialPrize}</td>
+                                <td style={{ border: "1px solid black" }} className="col2">{countSpecialPrizes[specialPrize]} ngày</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </Container>
         </section>
     );
