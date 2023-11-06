8e3f5e728e7439870161dbcda4ae059b504fca2b
nguyen ha trang <trangnthhe163554@fpt.edu.vn>
2023-11-07 00:20:45 +0700

    top number fe

--- frontend/src/Pages/General/General.jsx
+++ frontend/src/Pages/General/General.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const General = () => {
     return (
         <Container>
-            <Row className="align-items-center">
-                <Col md={12} lg={12} sm={12} className="text-center mb-5">
-                    <div className="section-title mt-5">
+            <Row>
+                <Col md={12} lg={12} sm={12}>
+                    <div className="text-left">
                         <h1>Thống Kê Tổng Hợp</h1>
                     </div>
-                </Col>
-                <Col>
                     <div>
-                    Bảng số Top ngày:&nbsp;<input type="date"></input>
-
-
+                    
                     </div>
+                    
+                        
+                </Col>
 
+                <Col md={12} lg={12} sm={12} className="text-center mb-5">
+                    <Link to="/"><button className="btn btn-warning">Go Back</button></Link>
                 </Col>
             </Row>
         </Container>
